48a469b8dfea594065e2416a2533448ac1821e47
Samantha Larson <squatnut@gmail.com>
2017-06-21 23:08:19 -0600

    Assignment4.2
    Samantha Larson

---
+++ htdocs/index.php
@@ -0,0 +1,12 @@
+<?php
+include_once "../mysql.php";
+include_once "../dbConnect.php";
+
+//fInsertToDatabase("B01KVKAE04", "The Cats Eye", 7.84);
+//fDeleteFromDatabase("B01KVKAE04");
+//fListFromDatabase();
+ActorfInsertToDatabase("the", "cat");
+ActorfDeleteFromDatabase("the", "cat");
+ActorfListFromDatabase();
+?>
+
--- mySQL.php
+++ mySQL.php
@@ -1,17 +1,63 @@
 <?php
 // database functions ************************************************
+    include "../dbConnect.php";
 
-function fInsertToDatabase() {
-  $sql = "INSERT INTO dvdtitles (asin, title, price) VALUES ('$asin', '$title', $price)";
-  // TODO: Fill in the rest of the function
+function fInsertToDatabase($asin, $title, $price) {
+    $myDB = fConnectToDatabase();
+    $sql = "INSERT INTO dvdtitles (asin, title, price) VALUES ('$asin', '$title', $price)";
+    $result = $myDB->prepare($sql);
+    $result->execute();
 }
 
-function fDeleteFromDatabase() {
-  $sql = "DELETE FROM tblCustomers WHERE CustID=$deleteID";
-  // TODO: Fill in the rest of the function
+function fDeleteFromDatabase($asin) {
+    $myDB = fConnectToDatabase();
+    $sql = "DELETE FROM dvdtitles WHERE asin='$asin'";
+    $result = $myDB->prepare($sql);
+    $result->execute();
+
 }
 
 function fListFromDatabase() {
-  $sql = 'SELECT custID, nameF, nameL FROM tblCustomers ORDER BY CustID';
-  // TODO: Fill in the rest of the fuction
+  $myDB = fConnectToDatabase();
+    $sql = 'SELECT asin, title, price FROM dvdtitles ORDER BY title';
+    $result = $myDB->prepare($sql);
+    $result->execute();
+
+    $set = $result->fetchAll();
+
+    foreach($set as $i=>$link ) {
+        echo $link["asin"] . " " . $link["title"] . " " . $link["price"] . " <br> " . "<img src=http://images.amazon.com/images/P/" . $link['asin'] . ".01.MZZZZZZZ.jpg />";
+        echo "<br>";
+    }
+
+}
+
+function ActorfInsertToDatabase($fname, $lname) {
+    $myDB = fConnectToDatabase();
+    $sql = "INSERT INTO dvdActors (fname, lname) VALUES ('$fname', '$lname')";
+    $result = $myDB->prepare($sql);
+    $result->execute();
+}
+
+function ActorfDeleteFromDatabase($fname, $lname) {
+    $myDB = fConnectToDatabase();
+    $sql = "DELETE FROM dvdActors WHERE fname='$fname' AND lname = '$lname'";
+    $result = $myDB->prepare($sql);
+    $result->execute();
+
+}
+
+function ActorfListFromDatabase() {
+    $myDB = fConnectToDatabase();
+    $sql = 'SELECT actorID, fname, lname FROM dvdActors ORDER BY actorID';
+    $result = $myDB->prepare($sql);
+    $result->execute();
+
+    $set = $result->fetchAll();
+
+    foreach($set as $i=>$link ) {
+        echo $link["actorID"] . " " . $link["fname"] . " " . $link["lname"];
+        echo "<br>";
+    }
+
 }
